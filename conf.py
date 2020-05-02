e77c7cf04d5ab070f7ead0463f1b30a23895dde2
dengbo <576696205@qq.com>
2020-05-02 22:23:15 +0800

    change back rst

--- conf.py
+++ conf.py
@@ -64,14 +64,3 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ['_static']
 
 master_doc = 'index'
-import recommonmark
-from recommonmark.transform import AutoStructify
-
-# At the bottom of conf.py
-github_doc_root = 'https://github.com/rtfd/recommonmark/tree/master/doc/'
-def setup(app):
-    app.add_config_value('recommonmark_config', {
-            'url_resolver': lambda url: github_doc_root + url,
-            'auto_toc_tree_section': 'Contents',
-            }, True)
-    app.add_transform(AutoStructify)
